04587b7b1aee75c213445c870c1e27f551dd3cbc
Saul Suazo <sa.suazom@duocuc.cl>
2021-10-15 17:45:40 -0300

    App moviles v4

--- src/app/pages/cambiomoneda/cambiomoneda.page.ts
+++ src/app/pages/cambiomoneda/cambiomoneda.page.ts
@@ -17,21 +17,42 @@ export class CambiomonedaPage {
   toVal1:any;
   fromVal1:any;
   toVal2:any;
-  fromVal2:any;
+  fromVal2: any;
 
   dolar:any;
   euro:any;
 
   constructor(private divisaServicio: DivisaService) { 
   }
+  onPeso(event){
+    console.log(event.target.value);
+    this.fromVal1=(event.target.value);
+    this.dePeso();
+  }
+  onDolar(event){
+    console.log(event.target.value);
+    this.toVal1=(event.target.value);
+    this.aDolar();
+  }
+
+  onPeso2(event){
+    console.log(event.target.value);
+    this.fromVal2=(event.target.value);
+    this.dePeso2();
+  }
+  onEuro(event){
+    console.log(event.target.value);
+    this.toVal2=(event.target.value);
+    this.aEuro();
+  }
+
+  
 
   ngOnInit() {
     this.cambioMoneda();
-    }
+  }
 
-
-
- async cambioMoneda(){
+  cambioMoneda(){
   this.divisaServicio.getMoneda()
   .subscribe(
     respuesta => {
@@ -47,7 +68,7 @@ export class CambiomonedaPage {
  }
 
  dePeso(){
-   this.toVal1 = this.fromVal1 / this.dolar;
+   this.toVal1 = Math.pow(this.fromVal1 / this.dolar,2);
  }
 
  aDolar(){
@@ -56,11 +77,19 @@ export class CambiomonedaPage {
 
  dePeso2(){
   this.toVal2 = this.fromVal2 / this.euro;
-}
+ }
  aEuro(){
   this.fromVal2 = this.toVal2 * this.euro;
  
-}
+ }
+ limpiarPesoUsd(){
+  this.fromVal1=0;
+  this.toVal1=0;
+ }
 
+ limpiarPesoEuro(){
+  this.fromVal2=0;
+  this.toVal2=0;
+ }
 
 }
--- src/app/services/divisa.service.ts
+++ src/app/services/divisa.service.ts
@@ -1,7 +1,5 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-//import { rejects } from 'assert';
-//import { resolve } from 'dns';
 
 @Injectable({
   providedIn: 'root'
